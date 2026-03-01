7f1810b214536a8e7f26509680ac296f488e9ee7
Khan072 <mdaabidkhan28@gmail.com>
2026-03-01 15:12:25 +0530

    minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'https://medicare-m3a0.onrender.com/',
+                target: 'http://localhost:5000',
                 changeOrigin: true,
             },
         },
